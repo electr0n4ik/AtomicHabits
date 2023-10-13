cfdee23a0744785e7c0e4600f69c8333a94f0f37
electr0n4ik <andreyshka3@gmail.com>
2023-10-13 15:54:13 +0300

    Add HabitViewSet.

--- config/urls.py
+++ config/urls.py
@@ -36,7 +36,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls', namespace='users')),
-    path('habits/', include('habits.urls', namespace='habits')),
+    path('habit/', include('main.urls', namespace='habit')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc')
 ]

6b4d1de547f941d40f7c4f6850c6413e81cc5836
electr0n4ik <andreyshka3@gmail.com>
2023-10-18 14:28:22 +0300

    fix

--- main/tests.py
+++ main/tests.py
@@ -1,4 +1,4 @@
-from rest_framework import status
+from rest_framework import response, status
 from rest_framework.test import APITestCase
 from rest_framework_simplejwt.tokens import RefreshToken
 
